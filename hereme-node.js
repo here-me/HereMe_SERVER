95a66a870764b3823d8df4001f54e80449397db4
namedboy <kkokey@gmail.com>
2019-06-02 02:29:18 +0900

    delete commaet

--- hereme-node.js
+++ hereme-node.js
@@ -16,20 +16,8 @@ if(day < 10){
 
 var today = year+""+month+""+day;
 
-console.log('today!!!!!z	',today)
-
-var apiUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=37.52&lon=127&units=metric&appid=a0094dc08d23f6c30af994855c039e73';
-
-var baseURL = 'http://api.openweathermap.org/data/2.5/weather';
-
 var API_KEY = 'FUyJph70P0YW0UHY5Ac9dqg08PRXqSoh05WS2SHPT%2FV0954wS1qpiR%2BdLt1djwmibD2O9QSwx1h%2FrLxEaWv%2FZg%3D%3D';
-var url = 'http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastTimeData';
-
-// default data is seoul
-var lat = '37.5667'; // 위도
-var lon = '126.9783'; // 경도
-
-var testUrl = baseURL + '' + lat + 
+var url = 'http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastGrib';
 
 // Index
 app.get('/',
@@ -60,10 +48,18 @@ app.get('/isHot/:nx/:ny',
 		url: url + queryParams,
 		method: 'GET'
 	}, function (error, response, body) {
-		var jsonBody = response.body;
+		var stringBody = response.body;
+		let jsonData = JSON.parse(stringBody);
+		
+		let resArr = jsonData.response.body.items.item;
+		for(var i=0; i<resArr.length; i++){
+			if(resArr[i].category === 'T1H'){
+				console.log(resArr[i].obsrValue);
+			}
+		}
 		
 		res.status(200);
-	  	res.json({success:true, message: 'data', 'body': JSON.parse(jsonBody) });
+	  	res.json({success:true, message: 'data', 'body': jsonData });
 	});
 	  
   }
@@ -73,94 +69,5 @@ var port = 8080;
 app.listen(port, function(){
   console.log('listening on port:' + port);
 });
-
-
-// // Show
-// router.get('/:id',
-//   function(req, res, next){
-//     Hero.findOne({id:req.params.id})
-//     .exec(function(err, hero){
-//       if(err) {
-//         res.status(500);
-//         res.json({success:false, message:err});
-//       }
-//       else if(!hero){
-//         res.json({success:false, message:"hero not found"});
-//       }
-//       else {
-//         res.json({success:true, data:hero});
-//       }
-//     });
-//   }
-// );
-
-// // Create
-// router.post('/',
-//   function(req, res, next){
-//     Hero.findOne({})
-//     .sort({id: -1})
-//     .exec(function(err, hero){
-//       if(err) {
-//         res.status(500);
-//         return res.json({success:false, message:err});
-//       }
-//       else {
-//         res.locals.lastId = hero?hero.id:0;
-//         next();
-//       }
-//     });
-//   },
-//   function(req, res, next){
-//     var newHero = new Hero(req.body);
-//     newHero.id = res.locals.lastId + 1;
-//     newHero.save(function(err, hero){
-//       if(err) {
-//         res.status(500);
-//         res.json({success:false, message:err});
-//       }
-//       else {
-//         res.json({success:true, data:hero});
-//       }
-//     });
-//   }
-// );
-
-// // Update
-// router.put('/:id',
-//   function(req, res, next){
-//     Hero.findOneAndUpdate({id:req.params.id}, req.body)
-//     .exec(function(err, hero){
-//       if(err) {
-//         res.status(500);
-//         res.json({success:false, message:err});
-//       }
-//       else if(!hero){
-//         res.json({success:false, message:"hero not found"});
-//       }
-//       else {
-//         res.json({success:true});
-//       }
-//     });
-//   }
-// );
-
-// // Destroy
-// router.delete('/:id',
-//   function(req, res, next){
-//     Hero.findOneAndRemove({id:req.params.id})
-//     .exec(function(err, hero){
-//       if(err) {
-//         res.status(500);
-//         res.json({success:false, message:err});
-//       }
-//       else if(!hero){
-//         res.json({success:false, message:"hero not found"});
-//       }
-//       else {
-//         res.json({success:true});
-//       }
-//     });
-//   }
-// );
 
 module.exports = router;
